509a9c4ffa996534dd17f7bd19b56bffae7ba2d9
galvessantos <gabrielww1@hotmail.com>
2025-03-20 12:16:13 -0300

    Desafio dias da semana

---
+++ Java/s4estruturasControle/desafioDiasSemana/src/main/java/org/application/Program.java
@@ -0,0 +1,47 @@
+package org.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        //Exercício que liga o dia da semana a um número utilizando os conceitos aprendidos na seção de Estruturas de Controle.
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        String diaSemana;
+        int num;
+
+        System.out.println("=======TRANSFORMANDO O DIA DA SEMANA EM UM NÚMERO=======");
+
+        System.out.print("Digite um dia da semana (Ex: Quarta) : ");
+        diaSemana = sc.nextLine();
+
+        if (diaSemana.equalsIgnoreCase("domingo")) {
+            System.out.println("Você digitou o 1° dia da semana!");
+        }
+        else if (diaSemana.equalsIgnoreCase("segunda")) {
+            System.out.println("Você digitou o 2° dia da semana!");
+        }
+        else if (diaSemana.equalsIgnoreCase("terça")) {
+            System.out.println("Você digitou o 3° dia da semana!");
+        }
+        else if (diaSemana.equalsIgnoreCase("quarta")) {
+            System.out.println("Você digitou o 4° dia da semana!");
+        }
+        else if (diaSemana.equalsIgnoreCase("quinta")) {
+            System.out.println("Você digitou o 5° dia da semana!");
+        }
+        else if (diaSemana.equalsIgnoreCase("sexta")) {
+            System.out.println("Você digitou o 6° dia da semana!");
+        }
+        else if (diaSemana.equalsIgnoreCase("sábado")) {
+            System.out.println("Você digitou o 7° dia da semana!");
+        }
+        
+        sc.close();
+    }
+}
